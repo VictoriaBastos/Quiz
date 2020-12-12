917bf1e0a43c37d181492222d53d6bfcdca6e3b8
Victoria Bastos <75148922+VictoriaBastos@users.noreply.github.com>
2020-12-12 09:19:33 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+
+function start(){
+  document.getElementById("initial-box").style.display="none";
+  document.getElementById("quiz1").style.visibility="visible";
+}
+
+function next12(){
+  document.getElementById("quiz1").style.display="none";
+  document.getElementById("quiz2").style.visibility="visible";
+}
+function next23(){
+  document.getElementById("quiz2").style.display="none";
+  document.getElementById("quiz3").style.visibility="visible";
+}
+function next34(){
+  document.getElementById("quiz3").style.display="none";
+  document.getElementById("quiz4").style.visibility="visible";
+}
+function next45(){
+  document.getElementById("quiz4").style.display="none";
+  document.getElementById("quiz5").style.visibility="visible";
+}
+
+
+function check(){
+  var answer1 = document.quiz1.answer1.value;
+  var answer2 = document.quiz2.answer2.value;
+  var answer3 = document.quiz3.answer3.value;
+  var answer4 = document.quiz4.answer4.value;
+  var answer5 = document.quiz5.answer5.value;
+  var correct = 0;
+
+    if (answer1 == "Cascading Style Sheets") {
+      correct++; 
+    }
+    if (answer2 == "background-color"){
+      correct++;
+    }
+    if (answer3 == "Organizing information on a webpage"){
+      correct++;
+    }
+
+    if (answer4 == "border-radius"){
+      correct++;
+    }
+
+    if (answer5 == "h1"){
+      correct++;
+    }
+
+  var message = ["Don't give up!", "Keep studying and try again!", "Great Job!"]
+  var gif = ["img/bad.gif", "img/avarage.gif","img/good.gif" ]
+      if  (correct < 1){
+          range = 0;
+      }
+    
+        if  (correct > 1 && correct < 5){
+          range = 1;
+      }
+
+      if (correct > 4){
+        range = 2;
+      }
+  
+  document.getElementById("quiz5").style.display="none";
+  document.getElementById("feedback").style.visibility="visible";
+  
+  document.getElementById("number-correct").innerHTML = "You got " + correct + " correct.";
+  document.getElementById("message").innerHTML = message[range];
+  document.getElementById("gif").src = gif[range];
+}
